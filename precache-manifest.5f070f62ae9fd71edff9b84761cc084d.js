0dbc52820cebd30bea526ce2622a9e84e231fcd0
Jane <jane.e.nath@gmail.com>
2020-03-26 14:34:56 -0700

    Updates

---
+++ precache-manifest.5f070f62ae9fd71edff9b84761cc084d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1829144d1c35c053dc2c5e3ca583f3a",
+    "url": "/DraggableLists/index.html"
+  },
+  {
+    "revision": "0d68fe19017cfdddfddc",
+    "url": "/DraggableLists/static/css/main.6180a713.chunk.css"
+  },
+  {
+    "revision": "5c530d811f225bb1beb7",
+    "url": "/DraggableLists/static/js/2.dcc4163b.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/DraggableLists/static/js/2.dcc4163b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d68fe19017cfdddfddc",
+    "url": "/DraggableLists/static/js/main.988e131c.chunk.js"
+  },
+  {
+    "revision": "3f1cd568a20a68c4c56f",
+    "url": "/DraggableLists/static/js/runtime-main.7895aef1.js"
+  }
+]);
